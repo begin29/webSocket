1ef4a159779183cc51f6ac2df954662ccd30e945
Vadym <vadim.motsuch@gmail.com>
2014-11-26 06:46:48 +0200

    add some functional to web sockets

--- app.rb
+++ app.rb
@@ -1,16 +1,37 @@
 require 'thin'
 require 'sinatra/base'
 require 'em-websocket'
-# require_relative 'redmond'
+require 'slim'
 
 EventMachine.run do
   class App < Sinatra::Base
+    configure do
+      set :server, %w[thin]
+    end
+
     get '/' do
       slim :index
     end
+
   end
 
-  # our WebSockets server logic will go here
+  # @clients = []
 
-  App.run! port: 3001
+  EM::WebSocket.start(:host => '0.0.0.0', :port => '3001') do |ws|
+    ws.onopen do |handshake|
+      # @clients = ws
+      ws.send "Connected to #{handshake.path}."
+    end
+
+    ws.onclose do |closed| 
+      p "Connection closed"
+    end
+
+    ws.onmessage do |msg|
+      puts "Recieved message: #{msg}"
+      ws.send "Pong: #{msg}"
+    end
+  end
+
+  # App.run! port: 3000
 end
