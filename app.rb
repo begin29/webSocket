e01d0037ab4997f236ec8c00deb15b82bb8d8922
Vadym <vadim.motsuch@gmail.com>
2014-12-02 01:16:18 +0200

    websocket add color

--- app.rb
+++ app.rb
@@ -5,84 +5,65 @@ require 'slim'
 set :server, 'thin'
 set :sockets, []
 
-get '/' do
-  if !request.websocket?
-    slim :index
-  else
-    request.websocket do |ws|
-      ws.onopen do
-        ws.send("Hello World!")
-        settings.sockets << ws
-      end
-      ws.onmessage do |msg|
-        EM.next_tick { settings.sockets.each{|s| s.send(msg) } }
-      end
-      ws.onclose do
-        warn("websocket closed")
-        settings.sockets.delete(ws)
-      end
-    end
-  end
-end
+# get '/' do
+#   if !request.websocket?
+#     slim :index
+#   else
+#     request.websocket do |ws|
+#       ws.onopen do
+#         ws.send("Hello World!")
+#         settings.sockets << ws
+#       end
+#       ws.onmessage do |msg|
+#         EM.next_tick { settings.sockets.each{|s| s.send(msg) } }
+#       end
+#       ws.onclose do
+#         warn("websocket closed")
+#         settings.sockets.delete(ws)
+#       end
+#     end
+#   end
+# end
 
-get '/one_message/:id' do
+get '/:id' do
   if !request.websocket?
     slim :one_chanel
   else
-      request.websocket do |ws|
-          channel = params[:id]
-          @con = {channel: channel, socket: ws}
-          ws.onopen do
-              ws.send("Hello World!")
-              settings.sockets << @con
-          end
-          ws.onmessage do |msg|
-              return_array = []
-              settings.sockets.each do |hash|
-                  #puts hash
-                  #puts hash['channel']
-                  if hash[:channel] == channel
-                      #puts hash[:socket]
-                      return_array << hash
-                      puts "Same channel"
-                      puts return_array
-                  else
-                      puts hash[:channel]
-                      puts channel
-                      puts "Not in same channel"
-                  end
-              end
-              EM.next_tick { return_array.each{|s| s[:socket].send(msg) } }
-          end
-          ws.onclose do
-              warn("websocket closed")
-              settings.sockets.each do |hash|
-                  if hash[:socket] == ws
-                      settings.sockets.delete(hash)
-                      puts "deleted"
-                  else
-                      puts "not deleted"
-                  end
-              end
-          end
-      end
-  end
-end
-
-get '/emulate' do
-  if !request.websocket?
-    slim :emulate
-  else
     request.websocket do |ws|
+      channel = params[:id]
+      @con = {channel: channel, socket: ws, color: params[:color] || 'black'}
       ws.onopen do
-        settings.sockets << ws
+        ws.send("Hello From WebSocket Server!")
+        settings.sockets << @con
       end
       ws.onmessage do |msg|
-        EM.next_tick { settings.sockets.each{|s| s.send(msg) } }
+        return_array = []
+        settings.sockets.each do |hash|
+          #puts hash
+          #puts hash['channel']
+          if hash[:channel] == channel
+            #puts hash[:socket]
+            return_array << hash
+            puts "Same channel"
+            # puts return_array
+          else
+            puts hash[:channel]
+            puts channel
+            puts "Not in same channel"
+          end
+        end
+        EM.next_tick { return_array.each{|s| s[:socket].send("<p style='color:##{s[:color]}'>#{msg}</p>") } }
       end
       ws.onclose do
         warn("websocket closed")
-        settings.sockets.delete(ws)
+        settings.sockets.each do |hash|
+          if hash[:socket] == ws
+            settings.sockets.delete(hash)
+            puts "deleted"
+          else
+            puts "not deleted"
+          end
+        end
       end
     end
   end
--- public/websocket.js
+++ public/websocket.js
@@ -1,20 +1,46 @@
 $( document ).ready(function() {
  var show = function(el){
-    return function(msg){ el.innerHTML = msg + '<br />' + el.innerHTML; }
+    return function(msg){ el.innerHTML = msg + el.innerHTML; }
   }(document.getElementById('msgs'));
+  
+  var ws ;
+  var client = $.now().toString();
+  var color;
+  var connect = function(b){
+    var random_val = Math.floor((Math.random() * 1000) + 3);
+    color = random_val + client.substr(client.length - 3);
+    b.onclick = function(){
+      ws       = new WebSocket('ws://' + window.location.host + window.location.pathname+ '?color='+color);
+      ws.onopen    = function()  { show('websocket opened'); };
+      ws.onclose   = function()  { show('websocket closed'); }
+      ws.onmessage = function(m) { show(m.data); };
+      $(this).hide();
+      $('#disconnect').show();
+      return false;
+    };
+  }(document.getElementById('connect') )
 
-  var ws       = new WebSocket('ws://' + window.location.host + window.location.pathname);
-  ws.onopen    = function()  { show('websocket opened'); };
-  ws.onclose   = function()  { show('websocket closed'); }
-  ws.onmessage = function(m) { show('websocket message: ' +  m.data); };
+  $('#disconnect').on('click', function(){
+    $(this).hide();
+    $('#connect').show();
+    ws.close();
+    return false;
+  });
 
   var sender = function(f){
     var input     = document.getElementById('input');
     input.onclick = function(){ input.value = "" };
     f.onsubmit    = function(){
-      ws.send(input.value);
-      input.value = "send a message";
+      if(typeof ws !== 'undefined')
+      {
+        send_object = ws.send(client+': ' + input.value);
+        input.value = "send a message";
+      }
+      else{
+        alert('please connect to WebSocket server');
+      };
       return false;
     }
   }(document.getElementById('form'));
+
 });
